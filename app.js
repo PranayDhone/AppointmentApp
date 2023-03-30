cc2daff29ea9db65efcc67e3c85521d7b4455046
PranayDhone <pranayadhone18@gmail.com>
2023-03-30 14:26:00 +0530

    changes color font

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('I love git')
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor='green';
+items[0].style.fontWeight='bold';
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
+
+//console.log(items.style.font='bold');
+
+//items.style.backgroundColor='#f4f4f4';
+
