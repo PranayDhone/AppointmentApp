0dd2d4d75fc4fa85d2793649182b1f1764abe310
PranayDhone <pranayadhone18@gmail.com>
2023-03-30 15:21:01 +0530

    className or Tagname

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+//GETELEMENTBY_CLASS_NAME
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
 items[2].style.backgroundColor='green';
-items[0].style.fontWeight='bold';
-items[1].style.fontWeight='bold';
-items[2].style.fontWeight='bold';
-items[3].style.fontWeight='bold';
 
-//console.log(items.style.font='bold');
+for(var i=0;i<=items.length;i++){
+    items[i].style.fontWeight='bold';
+    items[i].style.backgroundColor='purple';
+}
 
-//items.style.backgroundColor='#f4f4f4';
+//GETELEMENTBY_TAG_NAME
+var li=document.getElementsByTagName('list-group-item');
+console.log(li);
+console.log(li[2]);
+li[2].style.backgroundColor='green';
+
+for(var i=0;i<=items.length;i++){
+    li[i].style.fontWeight='bold';
+    //li[i].style.backgroundColor='purple';
+}
 
